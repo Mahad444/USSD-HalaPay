9ab25ab55ebd2fae99fb0aae8d1103300ab4478b
Ally Juma Said <mahadsaid95@icloud.com>
2025-12-14 19:27:12 -0700

    Add utility class for USSD menu navigation

---
+++ util.php
@@ -0,0 +1,17 @@
+<?php
+class Util {
+    // utility functions can be added here in future
+    // About USSD menu
+    static $GO_BACK = "98";
+    static $GO_BACK_TO_MAIN_MENU = "99";
+}
+
+
+
+
+
+
+
+
+
+?>
