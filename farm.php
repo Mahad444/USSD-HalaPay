2479c57a2d44c1bc785e3e6dba25340fcf556900
Ally Juma Said <mahadsaid95@icloud.com>
2025-12-16 18:46:53 -0700

    Add farm.php for farmers' registration platform implementation

---
+++ farm.php
@@ -0,0 +1,34 @@
+<!-- Farmers' Registration Platform -->
+ <!-- Requirements For registration KYC -->
+  <!-- 1. Valid ID (National ID, Passport) -->
+   <!-- 2. Farm Details (Location, Size, Type of Crops) -->
+    <!-- 3. Contact Information (Phone Number, Email Address) -->
+     <!-- 4. PIN to login -->
+      <!-- 5. Confirmation of registration -->
+<?php
+
+
+// Register Farmer
+// Read the variables sent via POST from our API
+// callback URl https://srv564-files.hstgr.io/4442363f430eb686/files/public_html/Farmer/index.php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
