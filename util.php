59d086e30b94672361f19103d810e8078267f613
Ally Juma Said <mahadsaid95@icloud.com>
2025-12-29 10:10:19 -0700

    Refactor USSD menu constants for clarity

--- util.php
+++ util.php
@@ -3,13 +3,8 @@ class Util {
     // utility functions can be added here in future
     // About USSD menu
     static $GO_BACK = "98";
-    static $GO_BACK_TO_MAIN_MENU = "99";
+    static $GO_TO_MAIN_MENU = "99";
 }
-
-
-
-
-
 
 
 
