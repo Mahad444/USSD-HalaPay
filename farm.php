13babbd8287fc65b09805d671aac00fdec5ca05c
Ally Juma Said <mahadsaid95@icloud.com>
2026-01-20 22:01:53 -0700

    docs: Add a reference URL comment to farm.php.

--- farm.php
+++ farm.php
@@ -1,10 +1,11 @@
 <!-- Farmers' Registration Platform -->
- <!-- Requirements For registration KYC -->
-  <!-- 1. Valid ID (National ID, Passport) -->
-   <!-- 2. Farm Details (Location, Size, Type of Crops) -->
-    <!-- 3. Contact Information (Phone Number, Email Address) -->
-     <!-- 4. PIN to login -->
-      <!-- 5. Confirmation of registration -->
+<!-- Requirements For registration KYC -->
+<!-- 1. Valid ID (National ID, Passport) -->
+<!-- 2. Farm Details (Location, Size, Type of Crops) -->
+<!-- 3. Contact Information (Phone Number, Email Address) -->
+<!-- 4. PIN to login -->
+<!-- 5. Confirmation of registration -->
+<!-- https://srv564-files.hstgr.io/4442363f430eb686/files/public_html/Farmer/index.php -->
 <?php
 
 
