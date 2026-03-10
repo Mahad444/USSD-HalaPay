d1a6efa6ae8d45749b6f72382e03348093c84f48
Ally Juma Said <mahadsaid95@icloud.com>
2026-03-09 21:30:33 -0700

    added

--- util.php
+++ util.php
@@ -3,8 +3,10 @@ class Util {
     // utility functions can be added here in future
     // About USSD menu
     // 98  - Go Back
+    // # - Go to main menu
     static $GO_BACK = "98";
     static $GO_TO_MAIN_MENU = "99";
+
 }
 
 
