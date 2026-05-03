007c98a6706c3fd9feb2f2f4c274a652e66481c8
Ally Juma Said <mahadsaid95@icloud.com>
2026-05-03 16:33:58 -0700

    refactor: add commented-out checkBalanceMenu function for future implementation

--- util.php
+++ util.php
@@ -6,6 +6,14 @@ class Util {
     // # - Go to main menu
     static $GO_BACK = "98";
     static $GO_TO_MAIN_MENU = "99";
+    // Check Balance , PIN
+    // public function checkBalanceMenu($textArray){
+    // $level = count ($textArray);
+    // if($level == 1){
+    //     echo "CON Enter PIN";
+    // }elseif($level == 2){
+    //     // Check PIN correctness etc
+    //     echo "END We are processing your request, You will recieve SMS shortly";
 
 }
 
